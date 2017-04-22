9fe8f7ffb9a3ef597b4511b961cc483bed38d9ef
Zachary Balda <zacharybalda23@gmail.com>
2017-04-21 21:23:41 -0400

    add delay between object creations for VisualGC

--- Java/SimpleTests.java
+++ Java/SimpleTests.java
@@ -6,8 +6,10 @@ class SimpleTests {
 
         SimpleTests test = new SimpleTests();
 
-        test.test1(100000);
-        test.test2(100000);
+        test.sleep(20000);
+
+        test.test1(2000000);
+        test.test2(2000000);
 
     }
 
@@ -20,11 +22,13 @@ class SimpleTests {
         for(int i = 0; i < n; i++){
             int temp = rand.nextInt(2147483647);
             myList.add(temp);
+            sleep(1);
         }
         System.out.println("Created list of " + n + " integers.");
         
         for(int i = 0; i < n; i++){
             myList.removeFirst();
+            sleep(1);
         }
         System.out.println("Removed " + n + " integers one by one.");
         
@@ -39,12 +43,23 @@ class SimpleTests {
         for(int i = 0; i < n; i++){
             int temp = rand.nextInt(2147483647);
             myList.add(temp);
+            sleep(1);
         }
         System.out.println("Created list of " + n + " integers (2nd time).");
         
         myList = null;
         System.out.println("Set list to null (removing entire list at once.)");
 
+    }
+
+    void sleep(int miliseconds){
+
+        try {
+            Thread.sleep(miliseconds);
+        } catch (InterruptedException e){
+            e.printStackTrace();
+        }
+    
     }
 
 }
