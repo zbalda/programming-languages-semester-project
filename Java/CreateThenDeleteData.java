83e79504bf3d3731c0adfafcbe763f61e1b4f8a2
Zachary Balda <zacharybalda23@gmail.com>
2017-04-22 16:49:51 -0400

    write tests for creating then deleting data

--- Java/CreateThenDeleteData.java
+++ Java/CreateThenDeleteData.java
@@ -1,11 +1,43 @@
-import java.util.*
+import java.util.*;
 
 public class CreateThenDeleteData {
 
     public static void main(String[] args){
 
         CreateThenDeleteData createThenDeleteData = new CreateThenDeleteData();
+        createThenDeleteData.createThenDeleteData(1000);
+        createThenDeleteData.createThenDeleteAllData(1000);
 
+    }
+
+    void createThenDeleteData(int amount){
+        
+        Random rand = new Random();
+        LinkedList myList = new LinkedList();
+        int tempInt;
+        for(int i = 0; i < amount; i++){
+            tempInt = rand.nextInt(2147483647);
+            Node temp = new Node(tempInt);
+            myList.insertAtBeginning(temp);
+        }
+        for(int i = 0; i < amount; i++){
+            myList.removeFirst();
+        }
+    
+    }
+
+    void createThenDeleteAllData(int amount){
+        
+        Random rand = new Random();
+        LinkedList myList = new LinkedList();
+        int tempInt;
+        for(int i = 0; i < amount; i++){
+            tempInt = rand.nextInt(2147483647);
+            Node temp = new Node(tempInt);
+            myList.insertAtBeginning(temp);
+        }
+        myList.clear();
+    
     }
 
 }
