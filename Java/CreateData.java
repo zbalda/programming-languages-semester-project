959e3fa6a03a69b36ce2011fea1d27735986c5f1
Zachary Balda <zacharybalda23@gmail.com>
2017-04-22 14:34:50 -0400

    test Linked List class

--- Java/CreateData.java
+++ Java/CreateData.java
@@ -5,6 +5,27 @@ public class CreateData {
     public static void main(String[] args){
 
         CreateData createData = new CreateData();
+        createData.createData(1000);
+
+    }
+
+    void createData(int amount){
+        
+        Random rand = new Random();
+        LinkedList myList = new LinkedList();
+        int tempInt;
+        for(int i = 0; i < amount; i++){
+            tempInt = rand.nextInt(2147483647);
+            Node temp = new Node(tempInt);
+            myList.insertAtBeginning(temp);
+        }
+
+        // TEMP TEST
+        Node temp = new Node();
+        myList.insertAtEnd(temp);
+        myList.removeFirst();
+        myList.removeLast();
+        myList.clear();
 
     }
 
