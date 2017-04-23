66ff2ff7a64518113d35b680a4ec1fa374335fe3
Zachary Balda <zacharybalda23@gmail.com>
2017-04-22 22:39:38 -0400

    add time delay between creations and deletions

--- Java/CreateData.java
+++ Java/CreateData.java
@@ -5,19 +5,30 @@ public class CreateData {
     public static void main(String[] args){
 
         CreateData createData = new CreateData();
-        createData.createData(1000);
+        createData.createData(10000, 1);
 
     }
 
-    void createData(int amount){
+    void createData(int amount, int delayMiliSec){
         
         Random rand = new Random();
         LinkedList myList = new LinkedList();
         int tempInt;
         for(int i = 0; i < amount; i++){
+            sleep(delayMiliSec);
             tempInt = rand.nextInt(2147483647);
             Node temp = new Node(tempInt);
             myList.insertAtBeginning(temp);
+        }
+    
+    }
+    
+    void sleep(int miliseconds){
+
+        try {
+            Thread.sleep(miliseconds);
+        } catch (InterruptedException e){
+            e.printStackTrace();
         }
     
     }
--- Java/CreateThenDeleteData.java
+++ Java/CreateThenDeleteData.java
@@ -5,38 +5,51 @@ public class CreateThenDeleteData {
     public static void main(String[] args){
 
         CreateThenDeleteData createThenDeleteData = new CreateThenDeleteData();
-        createThenDeleteData.createThenDeleteData(1000);
-        createThenDeleteData.createThenDeleteAllData(1000);
+        createThenDeleteData.createThenDeleteData(10000, 1);
+        createThenDeleteData.createThenDeleteAllData(10000, 1);
 
     }
 
-    void createThenDeleteData(int amount){
+    void createThenDeleteData(int amount, int delayMiliSec){
         
         Random rand = new Random();
         LinkedList myList = new LinkedList();
         int tempInt;
         for(int i = 0; i < amount; i++){
+            sleep(delayMiliSec);
             tempInt = rand.nextInt(2147483647);
             Node temp = new Node(tempInt);
             myList.insertAtBeginning(temp);
         }
         for(int i = 0; i < amount; i++){
+            sleep(delayMiliSec);
             myList.removeFirst();
         }
     
     }
 
-    void createThenDeleteAllData(int amount){
+    void createThenDeleteAllData(int amount, int delayMiliSec){
         
         Random rand = new Random();
         LinkedList myList = new LinkedList();
         int tempInt;
         for(int i = 0; i < amount; i++){
+            sleep(delayMiliSec);
             tempInt = rand.nextInt(2147483647);
             Node temp = new Node(tempInt);
             myList.insertAtBeginning(temp);
         }
         myList.clear();
+    
+    }
+    
+    void sleep(int miliseconds){
+
+        try {
+            Thread.sleep(miliseconds);
+        } catch (InterruptedException e){
+            e.printStackTrace();
+        }
     
     }
 
