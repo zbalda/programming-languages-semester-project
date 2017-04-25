bc00ce042218299d932f20ee6da2ab04bda9236c
Zachary Balda <zacharybalda23@gmail.com>
2017-04-25 01:00:49 -0400

    create 2D list structure

--- Java/LinkedList2D.java
+++ Java/LinkedList2D.java
@@ -1,29 +1,29 @@
-public class LinkedList {
+public class LinkedList2D {
 
-    private Node first;
+    private Node2D first;
 
     /* default constructor: list is empty */
-    LinkedList(){
+    LinkedList2D(){
         this.first = null;
     }
 
     /* overloaded constructor: list is set for an intial first node */
-    LinkedList(Node first){
+    LinkedList2D(Node2D first){
         this.first = first;
     }
 
     /* inserts a node at the beginning of the list */
-    public void insertAtBeginning(Node node){
+    public void insertAtBeginning(Node2D node){
         node.setNextNode(first);
         first = node;
     }
 
     /* inserts a node at the end of the list */
-    public void insertAtEnd(Node node) {
+    public void insertAtEnd(Node2D node) {
         if(first == null) {
             first = node;
         } else {
-            Node temp = first;
+            Node2D temp = first;
             while(temp.getNextNode() != null) {
                 temp = temp.getNextNode();
             }
@@ -32,27 +32,27 @@ public class LinkedList {
     }
 
     /* deletes the first node in the list */
-    public Node removeFirst(){
+    public Node2D removeFirst(){
         if(first == null){
             return null;
         } else {
-            Node temp = first;
+            Node2D temp = first;
             first = first.getNextNode();
             return temp;
         }
     }
 
     /* deletes the last node in the list */
-    public Node removeLast(){
+    public Node2D removeLast(){
         if(first == null){
             return null;
         } else if (first.getNextNode() == null){
-            Node temp = first;
+            Node2D temp = first;
             first = null;
             return temp;
         } else {
-            Node temp1 = first;
-            Node temp2 = first.getNextNode();
+            Node2D temp1 = first;
+            Node2D temp2 = first.getNextNode();
             while(temp2.getNextNode() != null){
                 temp1 = temp2;
                 temp2 = temp2.getNextNode();
--- Java/Node2D.java
+++ Java/Node2D.java
@@ -1,22 +1,46 @@
-public class Node {
+public class Node2D {
 
-    private Node nextNode;
-    private int someNumber;
+    private Node2D nextNode;
+    private LinkedList list;
 
-    Node(){
-        someNumber = 516;
+    Node2D(){
+        this.list = new LinkedList();
     }
 
-    Node(int someNumber){
-        this.someNumber = someNumber;
+    Node2D(LinkedList list){
+        this.list = list;
     }
 
-    public Node getNextNode(){
+    public Node2D getNextNode(){
         return this.nextNode;
     }
 
-    public void setNextNode(Node nextNode){
+    public void setNextNode(Node2D nextNode){
         this.nextNode = nextNode;
+    }
+    
+    /* populates Node2D with list of given size */
+    public void populateList(int size){
+        Random rand = new Random();
+        int tempInt;
+        for(int i = 0; i < size; i++){
+            tempInt = rand.nextInt(2147483647);   
+            Node temp = new Node(tempInt);
+            list.insertAtBeginning(temp);
+        }
+    }
+
+    /* populates Node2D with cycle of given size */
+    public void populateCycle(int size){
+        Random rand = new Random();
+        int tempInt;
+        for(int i = 0; i < size-1; i++){
+            tempInt = rand.nextInt(2147483647);   
+            Node temp = new Node(tempInt);
+            list.insertAtBeginning(temp);
+        }
+        Node temp = new Node(516);
+        list.insertCycle(temp);
     }
 
 }
