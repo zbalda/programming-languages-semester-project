bac5e0ff78f78fe81fe051f53aa9b3064a9a2e9e
Zachary Balda <zacharybalda23@gmail.com>
2017-04-30 00:21:40 -0400

    complete Java scheduled service test

--- Java/ScheduledService.java
+++ Java/ScheduledService.java
@@ -1,4 +1,4 @@
-import java.util.concurrent.Executers;
+import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
@@ -29,28 +29,40 @@ class Service extends Thread {
     public void run(){
 
         System.out.println("Running " + threadName + " for " + serviceType);
-        try {
+        // try {
             if(serviceType == 0){
-                ScheduledExecutorService execService = Executers.newScheduledThreadPool(5);
+                ScheduledExecutorService execService = Executors.newScheduledThreadPool(5);
                 execService.scheduleAtFixedRate(()->{
-                    System.out.println("Service run at: " + new java.util.Date());
-                    }, 0, 100L, TimeUnit.MILISECONDS);
+                    System.out.println(System.currentTimeMillis());
+                    }, 0, 10L, TimeUnit.MILLISECONDS);
             } else if(serviceType == 1){
                 CreateData cd = new CreateData();
+                cd.createData(100000, 1);
+                System.out.println("Create Data test finished.");
             } else if(serviceType == 2){
                 CreateThenDeleteData ctdd = new CreateThenDeleteData();
+                ctdd.createThenDeleteData(100000, 1);
+                ctdd.createThenDeleteAllData(100000, 1);
+                System.out.println("Create then Delete Data test finished.");
             } else if(serviceType == 3){
                 CreateAndDeleteNodes cadn = new CreateAndDeleteNodes();
+                cadn.createAndDeleteNodes(10000, 100000, 1, 2);
+                System.out.println("Create and Delete Nodes test finished.");
             } else if(serviceType == 4){
                 CreateAndDeleteLists cadl = new CreateAndDeleteLists();
-            } else if(serviceTpye == 5){
+                cadl.createAndDeleteLists(10000, 100, 100000, 1, 2);
+                System.out.println("Create and Delete Lists test finished.");
+            } else if(serviceType == 5){
                 CreateAndDeleteCycles cadc = new CreateAndDeleteCycles();
+                cadc.createAndDeleteCycles(10000, 100, 100000, 1, 2);
+                System.out.println("Create and Delete Cycles test finished.");
             } else {
                 System.out.println(serviceType + " is not a valid service type input");
             }
-        } catch (InterruptedException e){
-            System.out.println("Thread " + threadName + " interrupted.");
-        }
+            // throw new MyInterruptedException("MyInterruptedException.");
+        // } catch (MyInterruptedException e){
+        //     System.out.println("Thread " + threadName + " interrupted.");
+        // }
         System.out.println("Thread " + threadName + " exiting.");
 
     }
@@ -70,7 +82,10 @@ public class ScheduledService {
     public static void main(String args[]){
 
         Service timer = new Service("Timer-thread", 0);
-        Service data = new Service("Data-thread", 4);
+        Service data = new Service("Data-thread", 4); // also test for 1-5
+
+        timer.start();
+        data.start();
 
     }
 
