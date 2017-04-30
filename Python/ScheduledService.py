a121416680c8109df592a67dadd65e5cff9472c5
Zachary Balda <zacharybalda23@gmail.com>
2017-04-30 16:17:58 -0400

    fix typos and add imports for others tests

--- Python/ScheduledService.py
+++ Python/ScheduledService.py
@@ -1,6 +1,12 @@
 import threading
 import time
 
+from CreateData import CreateData
+from CreateThenDeleteData import CreateThenDeleteData
+from CreateAndDeleteNodes import CreateAndDeleteNodes
+from CreateAndDeleteLists import CreateAndDeleteLists
+from CreateAndDeleteCycles import CreateAndDeleteCycles
+
 exitFlag = 0
 
 class ScheduledService (threading.Thread):
@@ -20,19 +26,19 @@ def test(testType, delay):
             print int(round(time.time() * 1000))
             time.sleep((delay/1000))
     elif testType == "CreateData":
-
+        cd = CreateData.initialize()
         print "Create Data test finished."
     elif testType == "CreateThenDeleteData":
-
+        ctdd = CreateThenDeleteData.initialize()
         print "Create then Delete Data test finished."
     elif testType == "CreateAndDeleteNodes":
-        
+        cadn = CreateAndDeleteNodes.initialize()
         print "Create and Delete Lists test finished."
     elif testType == "CreateAndDeleteLists":
         cadl = CreateAndDeleteLists.initialize()
         print "Create and Delete Lists test finished."
     elif testType == "CreateAndDeleteCycles":
-        
+        cadc = CreateAndDeleteCycles.initialize()
         print "Create and Delete Cycles test finished."
     else: 
         print "invalid test type input"
@@ -43,6 +49,6 @@ dataThread = ScheduledService(2, "CreateAndDeleteLists", 10)
 
 # Start new Threads
 timerThread.start()
-timerThread.start()
+dataThread.start()
 
 print "Exiting Main Thread"
